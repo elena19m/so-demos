66bbc9b1e8281430a8eaef310c2cda79994573ab
Elena Mihailescu <elenamihailescu22@gmail.com>
2022-03-09 13:05:38 +0200

    [Lab01] Add makefile and test file for step by step compiling
    Signed-off-by: Elena Mihailescu <elenamihailescu22@gmail.com>

---
+++ lab01/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "test.h"
+
+#define A 4
+#define SO "This is SO!"
+
+int main()
+{
+	int b;
+
+	b = A + 2;
+	printf("b is %d\n", b);
+	printf("C is %d\n", C);
+	printf("SO is %s\n", SO);
+	printf("string is %s\n", string);
+
+	return 0;
+}
---
+++ lab01/test.h
@@ -0,0 +1,8 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#define C 2
+
+char *string = "This is SO, too!";
+
+#endif
